8a729c6c4d7f5cc3903d6f44e90add9b562ac0f8
Yorqinali <grose20030916@gmail.com>
2023-02-24 09:33:39 +0500

    1-4

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1.A va B butun sonlari yig'indisini hisoblash kerak bo'ladi
+
+// let a = 10;
+// let b = 20
+
+// console.log(a + b);
+
+// 2.Sonlar ustida amallarning eng muximlaridan biri bu - taqqoslashdir. Ushbu masalada sizga qo'yilgan talab, ikkita butun sonni taqqoslash kerak bo'ladi
+
+// let a = 20;
+// let b = 20;
+
+// if (a > b){
+//   console.log( '>' );
+// }else if(a < b){
+//   console.log('<');
+// }else if(a = b){
+//   console.log('=');
+// }
+
+// 3.Ikkita butun son A va B ning yig'indisini hisoblang
+
+// let a = 10;
+// let b = 20;
+//  if(a > 0 && b > 0){
+//   console.log(a + b);
+//  }else{
+//   console.log(false);
+//  }
+
+
+// 4.Tez orada RoboContest tizimining qanday ishlayotganligini tekshirish uchun RoboticsLab direktori ishxonamizga tashrif buyuradi. Biz direktorni tantanali kutib olish maqsadida direktorning moshinadan tushgan joyidan ishxonamiz eshigigacha bo'lgan oraliqga gilam to'shashga qaror qildik. Bizga ma'lumki direktorning mashinasi to'xtatiladigan joydan ishxonamiz kirish eshigigacha bo'lgan masofa N metr, bozorda 1 metr gilamning narxi P so'm. Biz gilam sotib olish uchun jami qancha mablag' sarflashimizni aniqlang!.
+
+// let metri = 11;
+// let narxi = 20000;
+
+// if(metri > 0 && narxi > 0){
+//   console.log(metri * narxi);
+// }else{
+//   console.log('olcham yoki narxni togri kiriting');
+// }
+
